214b940c9ed3d0e349bb6b8a45d9d22cbd1a2d9c
JFONLI <yenfonglijfon@gmail.com>
2022-12-08 19:48:36 -0800

    Modify directory

--- IE6600_Final/all_exercise/exercise-6/global.R
+++
@@ -1,9 +0,0 @@
-library(shiny)
-library(dplyr)
-library(DT)
-library(shinyWidgets)
-library(plotly)
-
-#df.students <- data.frame(Course=as.character(),Country=as.character(), Students=as.numeric(), Gender=as.character())
-#write.csv(df.students, df.path)
-df.path <- file.path("~/Desktop/visualizationMaterials/data/students.csv")
--- IE6600_Final/all_exercise/exercise-7/global.R
+++
@@ -1,8 +0,0 @@
-library(shiny)
-library(dplyr)
-library(DT)
-library(shinyWidgets)
-library(plotly)
-
-default.df <- as.data.frame(data(package = "datasets")["results"])
-database.default <- as.character(default.df$results.Item)
